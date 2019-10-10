39560709f34d0aef19335d8e450a3ecd648baed0
Appolo-11 <evgeshik816@gmail.com>
2019-10-10 18:39:51 +0300

    Add users to laptop access table

--- philipp_app/views.py
+++ philipp_app/views.py
@@ -11,7 +11,21 @@ def access_laptop_list(request):
 def menu(request):
 	return render(request, 'philipp_app/menu.html')
 def new_user(request):
-	form = AccessTableForm()
+	if request.method == "POST":
+		form = AccessTableForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			if post.name == 'Alex':
+				print("Ama ALEX")
+			#if len(AccessTable.objects.filter(name=post.name).filter(last_name=post.last_name)) > 1:
+			post.card_number = AccessTable.objects.filter(name=post.name).filter(last_name=post.last_name)[0].card_number
+			post.sap_id = AccessTable.objects.filter(name=post.name).filter(last_name=post.last_name)[0].sap_id
+			post.laptop_access = True
+			post.terminal_access = False			
+			post.save()
+			print(post.sap_id)
+            		#return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
 	return render(request, 'philipp_app/user_edit.html', {'form': form})
-
 
