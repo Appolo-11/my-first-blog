c7675a36a97a0e7558aaf8802c6f62aa65daba72
kurbobo <kurbobo@yandex.ru>
2019-10-10 16:03:19 +0300

    added access_laptop_list

--- philipp_app/views.py
+++ philipp_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import AccessTable
 
 def access_table_list(request):
-	users = AccessTable.objects.order_by('name')	
+	users = AccessTable.objects.order_by('last_name')	
 	return render(request, 'philipp_app/access_table_list.html', {'users': users})
 def access_laptop_list(request):
-	users = AccessTable.objects.order_by('name')
-	return render(request, 'philipp_app/access_table_list.html', {'users': users})
+	users = AccessTable.objects.order_by('last_name')
+	return render(request, 'philipp_app/access_laptop_list.html', {'users': users})
 def menu(request):
 	return render(request, 'philipp_app/menu.html')
