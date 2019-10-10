399ded8a8cf0fe26b304b59f448391f1f28e2a96
kurbobo <kurbobo@yandex.ru>
2019-10-10 12:59:50 +0300

    added css and ability to put python elements in html

--- philipp_app/views.py
+++ philipp_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import AccessTable
 
 def access_table_list(request):
-    return render(request, 'philipp_app/access_table_list.html', {})
+	users = AccessTable.objects.order_by('name')	
+	return render(request, 'philipp_app/access_table_list.html', {'users': users})
