fe7ca8b5b42bfa184f2f9ea41441b701b0dfb35c
kurbobo <kurbobo@yandex.ru>
2019-10-12 19:18:24 +0300

    sap_id and laptop number made unique

--- philipp_app/models.py
+++ philipp_app/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 class AccessTable(models.Model):
     #worker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ST = 'ST'
+    OH = 'OH'
+    WT = 'WT'
     LAPTOP_STATUS_CHOICES = [
     ('OH', 'On hands'),
     ('ST', 'In storage'),
@@ -13,15 +15,15 @@ class AccessTable(models.Model):
 ]
     name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length=100)
-    card_number = models.IntegerField() #models.CharField(max_length=50)
+    card_number = models.IntegerField(unique=True) #models.CharField(max_length=50)
     sap_id = models.CharField(max_length=50, unique=True)
     laptop_access = models.BooleanField()
     terminal_access = models.BooleanField()
-    laptop_number = models.IntegerField()
-    laptop_status = models.CharField(max_length=50, choices=LAPTOP_STATUS_CHOICES, default=ST,)
+    # laptop_number = models.IntegerField()
+    # laptop_status = models.CharField(max_length=50, choices=LAPTOP_STATUS_CHOICES, default=ST,)
     if laptop_access:
-    	laptop_number = models.IntegerField()
-    	laptop_status = models.CharField(max_length=50, choices=LAPTOP_STATUS_CHOICES, default=ST,)
+    	laptop_number = models.IntegerField(unique=True)
+    	laptop_status = models.CharField(max_length=50, choices=LAPTOP_STATUS_CHOICES, default=ST,unique=True)
     def publish(self):
         self.save()
 
